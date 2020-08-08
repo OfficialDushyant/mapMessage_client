491526753fdc9d40a5b9e7650b919f74fe1db885
Dushyant Patel <developerdushyant@gmail.com>
2020-08-08 20:09:40 +0530

    Initial load of map 🗺

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import React, {Component} from 'react';
 import './App.css';
+import {Map, TileLayer,Marker, Popup} from 'react-leaflet'
 
 class App extends Component {
+  state = {
+    lat: 51.505,
+    lng: -0.09,
+    zoom: 13,
+  }
   render(){
+    const position = [this.state.lat, this.state.lng]
     return(
-      <div className="App">
-        <h1>Hello, World 🗺</h1>
-      </div>
+      <Map className="map" center={position} zoom={this.state.zoom}>
+        <TileLayer
+          attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={position}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </Map>
     );
   }
 }
